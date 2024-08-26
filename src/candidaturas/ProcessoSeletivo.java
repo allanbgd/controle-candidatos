a9aaac4bd5d4c9fc992b843f2f537ca554ba9028
Allan Breno <allanbrenogomesdias@gmail.com>
2024-08-25 22:15:35 -0300

    novas funcionalidades

--- src/candidaturas/ProcessoSeletivo.java
+++ src/candidaturas/ProcessoSeletivo.java
@@ -1,14 +1,43 @@
 package candidaturas;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
 	public static void main(String[] args) {
 		
-		analizarCandidato(1900.0);
-		analizarCandidato(2200.0);
-		analizarCandidato(2000.0);
+	selecaoCandidatos();
 	}
 	
+	static void selecaoCandidatos() {
+		
+		String [] candidatos= {"Ana", "Bruno", "Carlos", "Diana", "Eduardo", "Fernanda", "Gabriel","Helena", "Igor", "Júlia"};
+		
+		int candidatosSelecionados = 0 ;
+		int candidatoAtual=0;
+		double salarioBase = 2000.0;
+		while(candidatosSelecionados< 5 && candidatoAtual < candidatos.length) {
+			
+			String candidato = candidatos[candidatoAtual];
+			double salarioPretendido= valorPretendido();
+			
+			System.out.println("O Candidato" + candidato + " solicitou este valor de salário: "+ salarioPretendido);
+			
+			if (salarioBase>=salarioPretendido) {
+				
+				System.out.println("O candidato "+ candidato + " foi selecionado para a vaga");
+				candidatosSelecionados++;
+						
+			}
+			candidatoAtual++;
+		}
+	}
+	static double valorPretendido() {
+		
+		return ThreadLocalRandom.current().nextDouble(1800,2200);
+		
+		
+	}
 	static void analizarCandidato(double salarioPretendido) {
 		
 		double salarioBase = 2000.0;
