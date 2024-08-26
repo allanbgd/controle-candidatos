481a21802dfb1da3ba1cc461488791c4a501f05b
Allan Breno <allanbrenogomesdias@gmail.com>
2024-08-25 21:44:05 -0300

    cria comparação de salários dos candidatos

--- src/candidaturas/ProcessoSeletivo.java
+++ src/candidaturas/ProcessoSeletivo.java
@@ -2,4 +2,32 @@ package candidaturas;
 
 public class ProcessoSeletivo {
 
+	public static void main(String[] args) {
+		
+		analizarCandidato(1900.0);
+		analizarCandidato(2200.0);
+		analizarCandidato(2000.0);
+	}
+	
+	static void analizarCandidato(double salarioPretendido) {
+		
+		double salarioBase = 2000.0;
+		
+		if (salarioBase > salarioPretendido) {
+			System.out.println("LIGAR PARA O CANDIDATO");
+			
+			
+			
+		}else if (salarioBase == salarioPretendido) {
+			
+			System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+		}else {
+			
+			System.out.println("AGUARDANDO O RESULTADO  DOS DEMAIS CANDIDATOS");
+		}
+		
+		
+		
+	}
+	
 }
